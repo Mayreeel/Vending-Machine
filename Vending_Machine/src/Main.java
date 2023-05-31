1ed26584715ca12c9d3f81a4e4c2270856d42985
Mayreeel <kuwon15@gmail.com>
2023-05-31 15:31:29 +0900

    feat : 메인

--- Vending_Machine/src/Main.java
+++ Vending_Machine/src/Main.java
@@ -1,6 +1,14 @@
-package src;
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        Change change = new Change();
+        MoneyInput moneyInput = new MoneyInput(change);
+
+        moneyInput.insertCoin(500);
+        moneyInput.insertCoin(1000);
+        moneyInput.insertCoin(50);
+        moneyInput.insertCoin(100);
+        moneyInput.insertCoin(5000);  // 유효하지 않은 화폐 단위
+
+        System.out.println("총 입력 금액: " + moneyInput.getTotalAmount());
     }
 }
